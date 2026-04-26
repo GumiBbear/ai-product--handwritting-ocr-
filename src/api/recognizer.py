301a3a817dfdd71dcf35df80b71ef8e0cd819978
solyanka55 <sshtangei@yandex.ru>
2026-04-26 17:42:57 +0300

    Update recognizer.py

--- src/api/recognizer.py
+++ src/api/recognizer.py
@@ -1,1 +1,12 @@
-
+class writingRecognizer:
+    
+    def __init__(self, model="model.pt"):
+        self.model = self.load_model(model)  
+        self.gpu_required = True
+    
+    def load_model(self, path):
+        pass
+    
+    def recognize(self, image_path):
+        result = self.model.predict(image_path)
+        return result  
